d08a51f2fc0e4e5823b2e09b5ae74920dc535b7c
Yarong Wang <wangyarong1266@gmail.com>
2022-10-19 18:59:36 -0700

    hw3-dfs

---
+++ src/java/edu/northeastern/hw3/Solution4.java
@@ -0,0 +1,37 @@
+package java.edu.northeastern.hw3;
+
+/**
+ * Word Search (https://leetcode.com/problems/word-search/)
+ */
+public class Solution4 {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length, cols = board[0].length;
+        if(word.length() == 0){ return true; }
+        if(rows * cols < word.length()){ return false; }
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                if(board[r][c] == word.charAt(0)){
+                    boolean isFound = dfsBoard(board, new boolean[rows][cols], r, c, word, 0);
+                    if(isFound){ return true; }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfsBoard(char[][] board, boolean[][] visitedBoard, int row, int col, String word, int wordInd) {
+        if( (row < 0 || row >= board.length)   ||   (col < 0 || col >= board[0].length)  ||   board[row][col] != word.charAt(wordInd) ){
+            return false;
+        }
+        if(visitedBoard[row][col] ){ return false; }
+        if(wordInd == word.length() - 1){ return true; }
+        visitedBoard[row][col] = true;
+
+        if( dfsBoard(board, visitedBoard, row, col-1, word, wordInd + 1) ){ return true; }
+        if( dfsBoard(board, visitedBoard, row, col+1, word, wordInd + 1) ){ return true; }
+        if( dfsBoard(board, visitedBoard, row-1, col, word, wordInd + 1) ){ return true; }
+        if( dfsBoard(board, visitedBoard, row+1, col, word, wordInd + 1) ){ return true; }
+        visitedBoard[row][col] = false;
+        return false;
+    }
+}
